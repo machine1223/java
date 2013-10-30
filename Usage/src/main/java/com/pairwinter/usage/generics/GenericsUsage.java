589547067e50e62f858a4894d1ea2d482848a360
damon <pairwinter@126.com>
2013-10-30 15:31:19 +0800

    upate

--- Usage/src/main/java/com/pairwinter/usage/generics/GenericsUsage.java
+++ Usage/src/main/java/com/pairwinter/usage/generics/GenericsUsage.java
@@ -3,6 +3,7 @@ package com.pairwinter.usage.generics;
 import com.pairwinter.usage.generics.fruit.Apple;
 import com.pairwinter.usage.generics.fruit.Banana;
 import com.pairwinter.usage.generics.fruit.Fruit;
+import com.pairwinter.usage.generics.fruit.GreenApple;
 import com.pairwinter.usage.generics.itface.GenericsInterfaceWithGenericsInInterfaceName;
 import com.pairwinter.usage.generics.itface.GenericsInterfaceWithOutGenericsInInterfaceName;
 import com.pairwinter.usage.generics.itfaceimpl.GenericsInterfaceImplWithGenericsInInterfaceName;
@@ -23,8 +24,8 @@ public class GenericsUsage {
         testGenericsOfClass();
         testGenericsOfInterface();
         testParentClassAndSubClassInList();
+        testParentClassAndSubClassInArray();
     }
-
     public static void testGenericsOfClass() {
         Result result = new Result();
         result.setResult("string");
@@ -60,6 +61,15 @@ public class GenericsUsage {
         System.out.println(result4_.getResult());
     }
 
+    /**
+     * 总结 ? extends 和 the ? super 通配符的特征，我们可以得出以下结论：<br/>
+     * 如果你想从一个数据类型里获取数据，使用 ? extends 通配符           <br/>
+     * 如果你想把对象写入一个数据结构里，使用 ? super 通配符             <br/>
+     * 如果你既想存，又想取，那就别用通配符。                            <br/>
+     * 这就是Maurice Naftalin在他的《Java Generics and Collections》这本书中所说的存取原则，
+     * 以及Joshua Bloch在他的《Effective Java》这本书中所说的PECS法则。
+     * Bloch提醒说，这PECS是指”Producer Extends, Consumer Super”，这个更容易记忆和运用。
+     */
     public static void testParentClassAndSubClassInList() {
         List list = new ArrayList();
         list.add("list string");
@@ -69,10 +79,43 @@ public class GenericsUsage {
         Fruit fruit = apple;
 
         List<Apple> appleList = new ArrayList<Apple>();
-//        下面的赋值操作编译不会通过，因为fruitList不能只放Apple
+//        下面的赋值操作编译不会通过，因为fruitList不能只放Apple,而且在处理泛型的时候也不会类型转换成功，比如不是Apple的类型不能转换为Apple
 //        List<Fruit> fruitList = appleList;
         List<Fruit> fruitList = new ArrayList<Fruit>();
         fruitList.add(new Apple());
         fruitList.add(new Banana());
+
+//        为了解决一个fruitList只能放Apple的编译问题，可以在泛型定义上进行处理,使用带有通配符的扩展声明。
+//        "<? extends Fruit>" 表示List中对象是Fruit的子类，但是无法确定具体是哪个子类。
+        List<? extends Fruit> listExtendsFruit = appleList;
+        Fruit fruit_ = new Fruit();
+//        但是下面操作会报编译错误，因为，给listExtendsFruit添加对象fruit_的时候，泛型不知道这个fruit_是Fruit还是Fruit的子对象，无法完成类型转换.
+//        通配符”extends“ 声明的对象无法添加任何对象。
+//        listExtendsFruit.add(fruit_);
+//        listExtendsFruit.add(new Apple());
+
+//        "<? super Fruit>" 表示List中对象是Apple的父类，但是无法确定具体是哪个父类。
+        List<? super Apple> listSuperApple = appleList;
+        listSuperApple.add(new Apple());
+        listSuperApple.add(new GreenApple());
+//        编译报错，因为Fruit不是Apple父类的子类，它是Apple的父类
+//        listSuperApple.add(new Fruit());
+        System.out.println("listSuperApple.size() ------> "+listSuperApple.size());
+
+        List<Fruit> fruits = new ArrayList<Fruit>();
+        listSuperApple = fruits;
+        listSuperApple.add(new Apple());
+    }
+
+    public static void testParentClassAndSubClassInArray(){
+        Apple[] apples = new Apple[5];
+//        下面的编译是可以通过的，因为这里不涉及泛型。所以编译没有问题，但在运行时会报异常ArrayStoreException
+//        这也证明泛型在类型安全检查方面有很好的作用，它可以在编译阶段就发现类型的不匹配。
+        Fruit[] fruits = apples;
+        try{
+            fruits[0] = new Banana();
+        }catch (ArrayStoreException ase){
+            System.out.println("fruits[0] = new Banana(); 这句话发生了ArrayStoreException异常");
+        }
     }
 }
---
+++ Usage/src/main/java/com/pairwinter/usage/generics/fruit/GreenApple.java
@@ -0,0 +1,11 @@
+package com.pairwinter.usage.generics.fruit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pairwinter
+ * Date: 13-10-30
+ * Time: 下午2:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class GreenApple extends Apple {
+}

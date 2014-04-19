7be99546dfc213bc0c13282bfbc3b38fa4f345db
damon <pairwinter@126.com>
2014-04-19 16:12:07 +0800

    update

--- Wendy/src/main/java/com/wendy/plsa/BuildKmeans.java
+++ Wendy/src/main/java/com/wendy/plsa/BuildKmeans.java
@@ -2,8 +2,12 @@ package com.wendy.plsa;
 
 import com.alibaba.simpleimage.analyze.sift.scale.KDFeaturePoint;
 import com.googlecode.javacv.cpp.opencv_core;
+import com.wendy.plsa.edu.Clusters;
+import com.wendy.plsa.edu.KMeansClusterer;
 import org.opencv.core.Core;
-
+import com.googlecode.javacv.cpp.opencv_core.CvMat;
+import com.googlecode.javacv.cpp.opencv_core.CvRNG;
+import com.googlecode.javacv.cpp.opencv_core.CvTermCriteria;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,21 +17,19 @@ import java.util.List;
 public class BuildKmeans {
     private final static String kmeans_result = "D:/workspace/dandan/result/plsa/sift/kmeans_result";
     public static void main(String[] args) throws Exception{
-        int k = 10;
+        int k = 2;
         List<KDFeaturePoint> features = BuildSift.parseAllImages();
         opencv_core.CvMat samples = opencv_core.CvMat.create(features.size(),128,opencv_core.CV_32FC1);
-        opencv_core.CvMat clusters = opencv_core.CvMat.create(features.size(),1,opencv_core.CV_32SC1);
-        opencv_core.CvMat centers = opencv_core.CvMat.create(k,128,opencv_core.CV_32FC1);
-        opencv_core.cvSetZero(clusters);
-        opencv_core.cvSetZero(centers);
+        int index = 0;
         for (KDFeaturePoint feature : features) {
+
             for(int i=0;i<feature.descriptor.length;i++){
-                samples.data_i().put(feature.descriptor[i]);
+                samples.data_fl().position(index).put(feature.descriptor[i]);
             }
+            index ++;
         }
-        double[] doubles = new double[]{};
-        opencv_core.cvKMeans2(samples, 10, clusters, opencv_core.cvTermCriteria(opencv_core.CV_TERMCRIT_EPS, 10, 1.0),
-                3,null, Core.KMEANS_USE_INITIAL_LABELS,centers,doubles);
-        opencv_core.cvSave(kmeans_result, clusters);
+        KMeansClusterer kMeansClusterer = new KMeansClusterer(k,1.0,10,3);
+        Clusters clusters1 = kMeansClusterer.cluster(samples);
+        opencv_core.cvSave(kmeans_result, clusters1.getClusterCenters());
     } 
 }

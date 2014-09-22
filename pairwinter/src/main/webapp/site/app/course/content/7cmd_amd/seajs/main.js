5d987663595316784b831f7dd59b65b179907432
damon <pairwinter@126.com>
2014-09-22 18:06:33 +0800

    update

--- pairwinter/src/main/webapp/site/app/course/content/7cmd_amd/seajs/main.js
+++ pairwinter/src/main/webapp/site/app/course/content/7cmd_amd/seajs/main.js
@@ -9,6 +9,12 @@
             c:'example/c',
             def:'example/def',
             'jquery':'/static/scripts/components/jquery/jquery'
+        },
+        vars:{
+            version:"sdfsdf"
+        },
+        map:{
+            ".js$":".js{{version}}"
         }
     });
 })();
